8a0cdbdc5d3bac803e8746dd2f2fba6ee96c939b
Lihini Nisansala <lihininisansala95@gmail.com>
2020-03-25 23:15:17 +0530

    add all operators

--- calc.py
+++ calc.py
@@ -1,4 +1,18 @@
 num1=int(input('Enter your first number:'))
 num2=int(input('Enter your second number:'))
 
+#addition
+print('{} + {} ='.format(num1, num2))
 print(num1+num2)
+
+#substraction
+print('{} + {} ='.format(num1, num2))
+print(num1-num2)
+
+#multiplication
+print('{} + {} ='.format(num1, num2))
+print(num1*num2)
+
+#division
+print('{} + {} ='.format(num1, num2))
+print(num1/num2) 
