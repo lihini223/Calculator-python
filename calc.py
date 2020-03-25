18a9f8aca6cf6142304ab3ed1e9a698daf808486
Lihini Nisansala <lihininisansala95@gmail.com>
2020-03-25 23:33:54 +0530

    add conditional statements

--- calc.py
+++ calc.py
@@ -1,18 +1,29 @@
+operation = input('''
+Please type the math operation you would like to complete:
++ for addition
+- for substraction
+* for multiplication
+/ for division
+''')
+
 num1=int(input('Enter your first number:'))
 num2=int(input('Enter your second number:'))
 
-#addition
-print('{} + {} ='.format(num1, num2))
-print(num1+num2)
+if operation == '+':#addition
+    print('{} + {} ='.format(num1, num2))
+    print(num1+num2)
 
-#substraction
-print('{} + {} ='.format(num1, num2))
-print(num1-num2)
+elif operation == '-':#substraction
+    print('{} - {} ='.format(num1, num2))
+    print(num1-num2)
 
-#multiplication
-print('{} + {} ='.format(num1, num2))
-print(num1*num2)
+elif operation == '*':#multiplication
+    print('{} * {} ='.format(num1, num2))
+    print(num1*num2)
 
-#division
-print('{} + {} ='.format(num1, num2))
-print(num1/num2) 
+elif operation == '/':#division
+    print('{} / {} ='.format(num1, num2))
+    print(num1/num2)
+    
+else:
+    print('Please enter a valid operator')
